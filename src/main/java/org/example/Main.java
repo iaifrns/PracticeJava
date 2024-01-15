251690cc95a215f37a5f3b8c30e45d568cdb195e
iaifrns <nsinifranc@gmail.com>
2024-01-15 12:07:08 +0100

    completed the making of numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,31 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
 
+    // this exercise is on a function maskNumbers that it main role is to mask a number except the last 4 digits
     public static void main(String[] args){
 
-      // there different exercises and each of them are on a particular branch
+      String[] listNumbers = {"9035847354", "894583486", "12", "23456", "3457657855"};
 
-        // 1 simpleCalculator : this branch contain a calculator code that perform 4 operation(+,-,/ and *)
+      for (int i=0; i<listNumbers.length; i++){
+          System.out.println(maskNumber(listNumbers[i]));
+      }
+    }
 
+    private static String maskNumber(String number){
 
+        int endPoint = number.length() - 4;
+
+        char[] num = number.toCharArray();
+
+        if(number.length() > 4){
+            Arrays.fill(num, 0, endPoint, '#');
+        }
+
+        return new String(num);
     }
 
 }
